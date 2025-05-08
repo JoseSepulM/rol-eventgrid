58a2ccfbbc1fa558fe6c77e9c5073ae8e197ecc5
JoseAseHoraDev <jom.sepul@asehora.com>
2025-05-08 14:20:46 -0400

    add Dockerfile

--- src/main/java/com/example/rol_eventgrid/controller/RolGraphQLController.java
+++ src/main/java/com/example/rol_eventgrid/controller/RolGraphQLController.java
@@ -5,6 +5,7 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
 import com.example.rol_eventgrid.DTO.RolUpdateRequest;
+import com.example.rol_eventgrid.DTO.RolCreateRequest;
 import com.example.rol_eventgrid.service.EventGridPublisherService;
 
 @Controller
@@ -18,5 +19,18 @@ public class RolGraphQLController {
         publisherService.publishEvent("RolActualizado", "/rol/" + id, input);
         return "Rol actualizado";
     }
+
+    @MutationMapping
+    public String createRol(@Argument RolCreateRequest input){
+        publisherService.publishEvent("RolCreado", "/rol", input);
+        return "Rol creado";
+    }
+
+    @MutationMapping
+    public String deleteRol(@Argument String id, @Argument String entity){
+        publisherService.publishEvent("RolEliminado", "/rol/" + id, entity);
+        return "Rol eliminado";
+    }
+
 
 }
